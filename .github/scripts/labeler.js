56cdbcb961760b7e0b5604adbb8808dd9ac063a0
Shiva <gsnrhyd6@gmail.com>
2023-05-23 17:35:25 +0530

    using github_token

--- .github/scripts/labeler.js
+++ .github/scripts/labeler.js
@@ -40,6 +40,7 @@ async function run() {
     // const token = core.getInput('repo-token', { required: true });
     const token = process.argv[2];
     console.log(process);
+    console.log(process.argv);
     const octokit = github.getOctokit(token);
 
     let labelsToAdd = [];
